1c0bcf7b84d2ac6fb111fe358f95ce88e615a6be
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2020-02-02 10:38:18 +0100

    Added addictions text field from slider

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet var colorView: UIView!
+	
+	@IBOutlet var redValueLabel: UILabel!
+	@IBOutlet var greenValueLabel: UILabel!
+	@IBOutlet var blueValueLabel: UILabel!
+	
+	@IBOutlet var redSlider: UISlider!
+	@IBOutlet var greenSlider: UISlider!
+	@IBOutlet var blueSlider: UISlider!
+	
+	@IBOutlet var redValue: UITextField!
+	@IBOutlet var greenValue: UITextField!
+	@IBOutlet var blueValue: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		colorView.layer.cornerRadius = 10
+		colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+		
+		redValueLabel.text = String(redSlider.value)
+		greenValueLabel.text = String(greenSlider.value)
+		blueValueLabel.text = String(blueSlider.value)
+		
+		redValue.text = redValueLabel.text
+		greenValue.text = greenValueLabel.text
+		blueValue.text = blueValueLabel.text
 	}
 
-
+	@IBAction func redSliderAction() {
+		redValueLabel.text = String(round(100*redSlider.value)/100)
+		colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+		redValue.text = String(round(100*redSlider.value)/100)
+	}
+	
+	@IBAction func greenSliderAction() {
+		greenValueLabel.text = String(round(100*greenSlider.value)/100)
+		colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+		greenValue.text = String(round(100*greenSlider.value)/100)
+	}
+	
+	@IBAction func blueSliderAction() {
+		blueValueLabel.text = String(round(100*blueSlider.value)/100)
+		colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+		blueValue.text = String(round(100*blueSlider.value)/100)
+		
+	}
+	
+	@IBAction func changeRedValue() {
+		redValueLabel.text = redValue.text
+	}
 }
 
